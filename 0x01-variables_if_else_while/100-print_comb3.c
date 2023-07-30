815c7e4e562e4d9c3f2b5da70bb4cd2cac6ec5a1
sydneytiyani <sydneytiyani@gmail.com>
2023-07-30 14:05:25 -0700

    Task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -4,27 +4,31 @@
  * main - Prints all possible combinations of two different digits,
  *        in ascending order, separated by a comma followed by a space.
  *
- * Return: Always 0.
- */
+ * Return: Always 0 (success)
+ *
+*/
 int main(void)
-{
-	int digit1, digit2;
 
-	for (digit1 = 0; digit1 < 9; digit1++)
+{
+	int ones = '0';
+	int tens = '0';
+
+	for (tens = '0'; tens <= '9'; tens++)
 	{
-		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
+		for (ones = '0'; ones <= '9'; ones++)
 		{
-			putchar((digit1 % 10) + '0');
-			putchar((digit2 % 10) + '0');
-			if (digit1 == 8 && digit2 == 9)
+			if (!((ones == tens) || (tens > ones)))
 			{
-				putchar(',');
-				putchar(' ');
+				putchar(tens);
+				putchar(ones);
+				if (!(ones == '9' && tens == '8'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
-
 	putchar('\n');
-
 	return (0);
 }
