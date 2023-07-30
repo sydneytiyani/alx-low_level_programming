c47eed40b1fef3dbd67a7377d440843e903070bc
sydneytiyani <sydneytiyani@gmail.com>
2023-07-30 16:03:23 -0700

    Task 1

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,8 @@
-#ifndef main_h
-#define main_h
+#ifndef MAIN_HEADER_FILE
+#define MAIN_HEADER_FILE
 
-int _putchar(char);
+int _putchar(char c);
+void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
