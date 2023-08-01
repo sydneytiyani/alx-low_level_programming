2b64102e72034d73b67068eee5292e2d446205e1
sydneytiyani <sydneytiyani@gmail.com>
2023-08-01 12:11:18 -0700

    Task 11

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -8,16 +8,25 @@
  */
 int main(void)
 {
-char c;
-int x;
+int pass[100];
+int i, sum, n;
 
-srand(time(0));
-while (x <= 2645)
+sum = o;
+
+srand(time(NULL));
+
+for (i = 0; i < 100; i++)
 {
-c = rand() % 128;
-x += c;
-putchar(c);
+	pass[i] = rand() % 78;
+	sum += (pass[i] + '0');
+	putchar(pass[i] + '0');
+	if ((2772 - sum) -'0' < 78)
+	{
+		n = 2772 - sum - '0';
+		sum += n;
+		putchar(n + '0');
+		break;
+	}
 }
-putchar(2772 - x);
 return (0);
 }
