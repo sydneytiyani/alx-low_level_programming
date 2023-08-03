ed325d6ca09bf39941d42478b138da306d98ddbc
sydneytiyani <sydneytiyani@gmail.com>
2023-08-03 11:38:17 -0700

    Task 104

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,78 +1,78 @@
-#include <stdio.h>
+#include "main.h"
 
-char *infinite_add(char *n1, char *n2, char *r, int size_r) {
-    int carry = 0; // Initialize carry to 0
-    int idx_n1 = 0;
-    int idx_n2 = 0;
-    int idx_r = 0;
+/**
+ * rev_string - reverse array
+ * @n: integer params
+ * Return: 0
+ */
 
-    // Find the lengths of the input strings
-    while (n1[idx_n1] != '\0')
-        idx_n1++;
-    while (n2[idx_n2] != '\0')
-        idx_n2++;
+void rev_string(char *n)
+{
+	int i = 0;
+	int j = 0;
+	char temp;
 
-    // Adding digits from the end to the beginning
-    while (idx_n1 > 0 || idx_n2 > 0) {
-        int digit1 = (idx_n1 > 0) ? (n1[idx_n1 - 1] - '0') : 0;
-        int digit2 = (idx_n2 > 0) ? (n2[idx_n2 - 1] - '0') : 0;
+	while (*(n + i) != '\0')
+	{
+		i++;
+	}
+	i--;
 
-        int sum = digit1 + digit2 + carry;
-        carry = sum / 10;
-        int digitSum = sum % 10;
-
-        if (idx_r < size_r - 1) {
-            r[idx_r] = digitSum + '0';
-            idx_r++;
-        } else {
-            // If the result can't be stored in r, return 0
-            return 0;
-        }
-
-        if (idx_n1 > 0)
-            idx_n1--;
-        if (idx_n2 > 0)
-            idx_n2--;
-    }
-
-    // If there's still a carry after processing all digits
-    if (carry > 0) {
-        if (idx_r < size_r - 1) {
-            r[idx_r] = carry + '0';
-            idx_r++;
-        } else {
-            return 0;
-        }
-    }
-
-    // Null-terminate the result string
-    r[idx_r] = '\0';
-
-    // Reverse the result string
-    int i = 0;
-    int j = idx_r - 1;
-    while (i < j) {
-        char temp = r[i];
-        r[i] = r[j];
-        r[j] = temp;
-        i++;
-        j--;
-    }
-
-    return r;
+	for (j = 0; j < i; j++, i--)
+	{
+		temp = *(n + j);
+		*(n + j) = *(n + i);
+		*(n + i) = temp;
+	}
 }
 
-int main(void) {
-    char *n = "1234567892434574367823574575678477685785645685876876774586734734563456453743756756784458";
-    char *m = "9034790663470697234682914569346259634958693246597324659762347956349265983465962349569346";
-    char r[100];
+/**
+ * infinite_add - add 2 numbers together
+ * @n1: text representation of 1st number to add
+ * @n2: text representation of 2nd number to add
+ * @r: pointer to buffer
+ * @size_r: buffer size
+ * Return: pointer to calling function
+ */
 
-    char *res = infinite_add(n, m, r, 100);
-    if (res == 0) {
-        printf("Error\n");
-    } else {
-        printf("%s + %s = %s\n", n, m, res);
-    }
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int overflow = 0, i = 0, j = 0, digits = 0;
+	int val1 = 0, val2 = 0, temp_tot = 0;
 
-    return 0;
+	while (*(n1 + i) != '\0')
+		i++;
+	while (*(n2 + j) != '\0')
+		j++;
+	i--;
+	j--;
+	if (j >= size_r || i >= size_r)
+		return (0);
+	while (j >= 0 || i >= 0 || overflow == 1)
+	{
+		if (i < 0)
+			val1 = 0;
+		else
+			val1 = *(n1 + i) - '0';
+		if (j < 0)
+			val2 = 0;
+		else
+			val2 = *(n2 + j) - '0';
+		temp_tot = val1 + val2 + overflow;
+		if (temp_tot >= 10)
+			overflow = 1;
+		else
+			overflow = 0;
+		if (digits >= (size_r - 1))
+			return (0);
+		*(r + digits) = (temp_tot % 10) + '0';
+		digits++;
+		j--;
+		i--;
+	}
+	if (digits == size_r)
+		return (0);
+	*(r + digits) = '\0';
+	rev_string(r);
+	return (r);
 }
