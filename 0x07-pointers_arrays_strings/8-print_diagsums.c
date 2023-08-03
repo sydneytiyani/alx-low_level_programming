a780560b4b876cacecf16a4fc2621a668c361a00
sydneytiyani <sydneytiyani@gmail.com>
2023-08-03 14:05:09 -0700

    Task 7

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,11 +1,13 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
  * print_diagsums - Entry point
  * @a: input
  * @size: input
  * Return: Always 0 (Success)
  */
+
 void print_diagsums(int *a, int size)
 {
 	int sum1, sum2, y;
